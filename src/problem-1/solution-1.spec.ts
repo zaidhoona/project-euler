003d37f61493448c256c91f5d1f3309eeb96e1f8
Zaid Hoona <zdhoona12@gmail.com>
2020-08-23 14:37:23 +0530

    Move the problem def to spec

--- src/problem-1/solution-1.spec.ts
+++ src/problem-1/solution-1.spec.ts
@@ -1,6 +1,14 @@
 import { Solution1 } from "./solution-1";
 
-describe('Sum of multiples of 3 and 5', () => {
+describe(`
+## Multiples of 3 and 5
+
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+Find the sum of all the multiples of 3 or 5 below 1000.
+
+_Answer: 233168_
+`, () => {
    let solution1: Solution1;
 
    beforeEach(() => {
